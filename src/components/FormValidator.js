4c3f5cf5d3674d4ea8c0ad01f2c420e797d64b14
Яна оман <yana.roman.8726@gmail.com>
2022-05-28 10:22:47 +0300

    удалить закомментированный код

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,210 +1,70 @@
-// export default class FormValidator {
-//     constructor(config, formElement) {
-//         this._inputSelector = config.inputSelector;
-//         this._submitButtonSelector = config.submitButtonSelector;
-//         this._inactiveButtonClass = config.inactiveButtonClass;
-//         this._inputErrorClass = config.inputErrorClass;
-//         this._errorClass = config.errorClass;
-//         this._formElement = formElement;
-//         this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
-//         this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector)); 
-//       }
-//     // Функция, которая добавляет класс с ошибкой
-//     _showInputError(inputElement) {
-//         const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
-//         inputElement.classList.add(this._inputErrorClass);
-//         errorElement.textContent = inputElement.validationMessage;
-//         errorElement.classList.add(this._errorClass);
-//       }
-//     // Функция, которая удаляет класс с ошибкой
-//     _hideInputError(inputElement) {
-//         const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
-//         inputElement.classList.remove(this._inputErrorClass);
-//         errorElement.classList.remove(this._errorClass);
-//         errorElement.textContent = '';
-//       }
-//     // Функция проверки валидности нескольких полей, принимает массив полей
-//     _hasInvalidInput(inputList) {
-//         return inputList.some((inputElement) => {
-//           return !inputElement.validity.valid;
-//         });
-//       };
-//     // Функция переключателя кнопки принимает массив полей ввода
-//     // и элемент кнопки, состояние которой нужно менять
-//     toggleButtonState() {
-//         if (this._hasInvalidInput(this._inputList)) {
-//           this._buttonElement.classList.add(this._inactiveButtonClass);
-//           this._buttonElement.disabled = true;
-//         } else {
-//           this._buttonElement.classList.remove(this._inactiveButtonClass);
-//           this._buttonElement.disabled = false;
-//         }
-//     };
+export default class FormValidator {
+    constructor(config, formElement) {
+        this._inputSelector = config.inputSelector;
+        this._submitButtonSelector = config.submitButtonSelector;
+        this._inactiveButtonClass = config.inactiveButtonClass;
+        this._inputErrorClass = config.inputErrorClass;
+        this._errorClass = config.errorClass;
+        this._formElement = formElement;
+        this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
+        this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector)); 
+      }
+    // Функция, которая добавляет класс с ошибкой
+    _showInputError(inputElement) {
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.add(this._inputErrorClass);
+        errorElement.textContent = inputElement.validationMessage;
+        errorElement.classList.add(this._errorClass);
+      }
+    // Функция, которая удаляет класс с ошибкой
+    _hideInputError(inputElement) {
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.remove(this._inputErrorClass);
+        errorElement.classList.remove(this._errorClass);
+        errorElement.textContent = '';
+      }
+    // Функция проверки валидности нескольких полей, принимает массив полей
+    _hasInvalidInput(inputList) {
+        return inputList.some((inputElement) => {
+          return !inputElement.validity.valid;
+        });
+      };
+    // Функция переключателя кнопки принимает массив полей ввода
+    // и элемент кнопки, состояние которой нужно менять
+    toggleButtonState() {
+        if (this._hasInvalidInput(this._inputList)) {
+          this._buttonElement.classList.add(this._inactiveButtonClass);
+          this._buttonElement.disabled = true;
+        } else {
+          this._buttonElement.classList.remove(this._inactiveButtonClass);
+          this._buttonElement.disabled = false;
+        }
+    };
     
-//     // Функция, которая проверяет валидность поля
-//     _isValid(inputElement) {
-//         if (inputElement.validity.valid) {
-//           this._hideInputError(inputElement);
-//         } else {
-//           this._showInputError(inputElement);
-//         }
-//       }
-//     //обработчики
-//     _setEventListeners = () => {
-//         //const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
-//         this.toggleButtonState();
-//         this._inputList.forEach((inputElement) => {
-//             inputElement.addEventListener('input', () => {
-//                 this._isValid(inputElement);
-//                 this.toggleButtonState();
-//       });
-//     });
-//       };
+    // Функция, которая проверяет валидность поля
+    _isValid(inputElement) {
+        if (inputElement.validity.valid) {
+          this._hideInputError(inputElement);
+        } else {
+          this._showInputError(inputElement);
+        }
+      }
+    //обработчики
+    _setEventListeners = () => {
+        //const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+        this.toggleButtonState();
+        this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener('input', () => {
+                this._isValid(inputElement);
+                this.toggleButtonState();
+      });
+    });
+      };
     
-//     enableValidation (){
-//         this._formElement.addEventListener('submit', (evt) => {
-//             evt.preventDefault();
-//           });
-//         this._setEventListeners();
-//     }
-// }
-
-export default class FormValidator { 
-
-  constructor(config, formElement) { 
-
-      this._inputSelector = config.inputSelector; 
-
-      this._submitButtonSelector = config.submitButtonSelector; 
-
-      this._inactiveButtonClass = config.inactiveButtonClass; 
-
-      this._inputErrorClass = config.inputErrorClass; 
-
-      this._errorClass = config.errorClass; 
-
-      this._formElement = formElement; 
-
-      this._buttonElement = this._formElement.querySelector(this._submitButtonSelector); 
-
-      this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));  
-
-    } 
-
-  // Функция, которая добавляет класс с ошибкой 
-
-  _showInputError(inputElement) { 
-
-      const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`); 
-
-      inputElement.classList.add(this._inputErrorClass); 
-
-      errorElement.textContent = inputElement.validationMessage; 
-
-      errorElement.classList.add(this._errorClass); 
-
-    } 
-
-  // Функция, которая удаляет класс с ошибкой 
-
-  _hideInputError(inputElement) { 
-
-      const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`); 
-
-      inputElement.classList.remove(this._inputErrorClass); 
-
-      errorElement.classList.remove(this._errorClass); 
-
-      errorElement.textContent = ''; 
-
-    } 
-
-  // Функция проверки валидности нескольких полей, принимает массив полей 
-
-  _hasInvalidInput(inputList) { 
-
-      return inputList.some((inputElement) => { 
-
-        return !inputElement.validity.valid; 
-
-      }); 
-
-    }; 
-
-  // Функция переключателя кнопки принимает массив полей ввода 
-
-  // и элемент кнопки, состояние которой нужно менять 
-
-  toggleButtonState() { 
-
-      if (this._hasInvalidInput(this._inputList)) { 
-
-        this._buttonElement.classList.add(this._inactiveButtonClass); 
-
-        this._buttonElement.disabled = true; 
-
-      } else { 
-
-        this._buttonElement.classList.remove(this._inactiveButtonClass); 
-
-        this._buttonElement.disabled = false; 
-
-      } 
-
-  }; 
-
-   
-
-  // Функция, которая проверяет валидность поля 
-
-  _isValid(inputElement) { 
-
-      if (inputElement.validity.valid) { 
-
-        this._hideInputError(inputElement); 
-
-      } else { 
-
-        this._showInputError(inputElement); 
-
-      } 
-
-    } 
-
-  //обработчики 
-
-  _setEventListeners = () => { 
-
-      const inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector)); 
-
-      this.toggleButtonState(inputList); 
-
-      inputList.forEach((inputElement) => { 
-
-          inputElement.addEventListener('input', () => { 
-
-              this._isValid(inputElement); 
-
-              this.toggleButtonState(inputList); 
-
-    }); 
-
-  }); 
-
-    }; 
-
-   
-
-  enableValidation (){ 
-
-      this._formElement.addEventListener('submit', (evt) => { 
-
-          evt.preventDefault(); 
-
-        }); 
-
-      this._setEventListeners(); 
-
-  } 
-
-} 
+    enableValidation (){
+        this._formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+          });
+        this._setEventListeners();
+    }
+}
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,128 +1,43 @@
-// export default class Popup {
-//     constructor(popupSelector) {
-//       this._popup = document.querySelector(popupSelector);
-//       this._popupBtnClose = this._popup.querySelector('.popup__close');
-//       this._handleEscCloseCallback = this._handleEscClose.bind(this);
-//       this._handleOverlayCloseCallback = this._handleOverlayClose.bind(this);
-//       this._handleCloseCallback = this.close.bind(this);
-//     }
+export default class Popup {
+    constructor(popupSelector) {
+      this._popup = document.querySelector(popupSelector);
+      this._popupBtnClose = this._popup.querySelector('.popup__close');
+      this._handleEscCloseCallback = this._handleEscClose.bind(this);
+      this._handleOverlayCloseCallback = this._handleOverlayClose.bind(this);
+      this._handleCloseCallback = this.close.bind(this);
+    }
   
-//     open() {
-//       this._popup.classList.add('popup_opened');
-//       this.setEventListeners();
-//     }
+    open() {
+      this._popup.classList.add('popup_opened');
+      this.setEventListeners();
+    }
   
-//     close() {
-//       this._popup.classList.remove('popup_opened');
-//       this.removeEventListeners();
-//     }
+    close() {
+      this._popup.classList.remove('popup_opened');
+      this.removeEventListeners();
+    }
   
-//     _handleEscClose(evt) {
-//       if (evt.key === 'Escape') {
-//         this.close();
-//       }
-//     }
+    _handleEscClose(evt) {
+      if (evt.key === 'Escape') {
+        this.close();
+      }
+    }
   
-//     _handleOverlayClose(evt) {
-//         if (evt.target.classList.contains('popup_opened')) {
-//           this.close();
-//         }
-//     }
+    _handleOverlayClose(evt) {
+        if (evt.target.classList.contains('popup_opened')) {
+          this.close();
+        }
+    }
   
-//     setEventListeners() {
-//       this._popupBtnClose.addEventListener('click', this._handleCloseCallback);
-//       document.addEventListener('keydown', this._handleEscCloseCallback);
-//       this._popup.addEventListener('click', this._handleOverlayCloseCallback);
-//     }
+    setEventListeners() {
+      this._popupBtnClose.addEventListener('click', this._handleCloseCallback);
+      document.addEventListener('keydown', this._handleEscCloseCallback);
+      this._popup.addEventListener('click', this._handleOverlayCloseCallback);
+    }
   
-//     removeEventListeners() {
-//       this._popupBtnClose.removeEventListener('click', this._handleCloseCallback);
-//       document.removeEventListener('keydown', this._handleEscCloseCallback);
-//       this._popup.removeEventListener('click', this._handleOverlayCloseCallback);
-//     }
-// }
-export default class Popup { 
-
-  constructor(popupSelector) { 
-
-    this._popup = document.querySelector(popupSelector); 
-
-    this._popupBtnClose = this._popup.querySelector('.popup__close'); 
-
-    this._handleEscCloseCallback = this._handleEscClose.bind(this); 
-
-    this._handleOverlayCloseCallback = this._handleOverlayClose.bind(this); 
-
-    this._handleCloseCallback = this.close.bind(this); 
-
-  } 
-
- 
-
-  open() { 
-
-    this._popup.classList.add('popup_opened'); 
-
-    this.setEventListeners(); 
-
-  } 
-
- 
-
-  close() { 
-
-    this._popup.classList.remove('popup_opened'); 
-
-    this.removeEventListeners(); 
-
-  } 
-
- 
-
-  _handleEscClose(evt) { 
-
-    if (evt.key === 'Escape') { 
-
-      this.close(); 
-
-    } 
-
-  } 
-
- 
-
-  _handleOverlayClose(evt) { 
-
-      if (evt.target.classList.contains('popup_opened')) { 
-
-        this.close(); 
-
-      } 
-
-  } 
-
- 
-
-  setEventListeners() { 
-
-    this._popupBtnClose.addEventListener('click', this._handleCloseCallback); 
-
-    document.addEventListener('keydown', this._handleEscCloseCallback); 
-
-    this._popup.addEventListener('click', this._handleOverlayCloseCallback); 
-
-  } 
-
- 
-
-  removeEventListeners() { 
-
-    this._popupBtnClose.removeEventListener('click', this._handleCloseCallback); 
-
-    document.removeEventListener('keydown', this._handleEscCloseCallback); 
-
-    this._popup.removeEventListener('click', this._handleOverlayCloseCallback); 
-
-  } 
-
-} 
+    removeEventListeners() {
+      this._popupBtnClose.removeEventListener('click', this._handleCloseCallback);
+      document.removeEventListener('keydown', this._handleEscCloseCallback);
+      this._popup.removeEventListener('click', this._handleOverlayCloseCallback);
+    }
+}
